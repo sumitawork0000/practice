be8f38d54cca1f51c7ade40c092354ba8538da7c
sumitawork0000 <sumita.work0000@gmail.com>
2025-09-18 16:26:38 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,13 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int prince;
+  private String color;
+  public TangibleAsset(String name, int prince, String color){
+    this.name = name;
+    this.prince = price;
+    this.color = color;
+  }
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
+}
